1ef2ecc599e08e874079c8ed9c502724033c6932
Sean Mitchuson <sean.mitchuson@gmail.com>
2026-02-04 20:48:29 -0500

    added fighter game

---
+++ fightstory.py
@@ -0,0 +1,12 @@
+fighter1_name = input("Name fighter 1: ")
+fighter2_name = input("Name fighter 2: ")
+fighter1_str = int(intput("Add a fighter stat: "))
+fighter2_str = int(input("Add an fighter stat: "))
+
+print(f"{fighter1_name} and {fighter2_name} have begun a battle!")
+if fighter1_str > fighter2_str:
+    print(f"{fighter1_name} has defeated {fighter2_name}")
+else:
+    print(f"{fighter2_name is victorious!}")
+
+    
