b8d796f16b3a5e7ed792f14c63eb2ff89fd1d074
Sean Mitchuson <sean.mitchuson@gmail.com>
2026-02-04 20:51:27 -0500

    added a default

---
+++ defaultmadlib.py
@@ -0,0 +1,7 @@
+character_one = input("Input a name: ")
+character_two = input("Input another name: ")
+action_1 = input("Enter an action: ")
+action_2 = input("Enter another action: ")
+
+print(f"""{character_one} once went to the forest.  There they {action_1} thinking no one was around, but {character_two} saw them doing {action_1}.
+      They were surprised by this and began to do {action_2} while seeing {character_one} still doing {action_1}.""")
